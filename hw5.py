599e7b12f6af89263a1b2f437d3f03c1f407a068
Yuan-Ping Chen <qoo0972@hotmail.com>
2017-11-12 18:56:45 -0800

    keras implementation

--- hw5.py
+++ hw5.py
@@ -71,7 +71,7 @@ def build_model_crnn():
     return model
 
 def write_result(predict, result_path):
-    d = 1-predicts
+    d = 1-predict
     h = np.expand_dims(predict, axis=1)
     d = np.expand_dims(d, axis=1)
     p = np.concantenate((d,h),axis=1)
